14014ade0c606685247ab740f7250dbed96472d8
Lane <llane@lockton.com>
2020-10-02 09:42:25 -0500

    debugging

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fetch = require("fetch");
+const fetch = require('axios');
 
 async function run() {
     try {
@@ -9,22 +9,28 @@ async function run() {
         // Get the JSON webhook payload for the event that triggered the workflow
         const payload = JSON.stringify(github.context.payload, undefined, 2)
 
+        // debugging only
+        const owner = 'lockton';
+        const repo = 'benefits';
+
+        console.log(payload);
+
         const GITHUB_TOKEN = core.getInput('token');
 
-        if (!process.env.INPUT_REPO) {
-            console.warn("no `repo` name given. fall-ing back to this repo");
-        }
+        // if (!process.env.INPUT_REPO) {
+        //     console.warn("no `repo` name given. fall-ing back to this repo");
+        // }
 
-        const [owner, repo] = (
-            process.env.INPUT_REPO || process.env.GITHUB_REPOSITORY
-        ).split("/");
+        // const [owner, repo] = (
+        //     process.env.INPUT_REPO || process.env.GITHUB_REPOSITORY
+        // ).split("/");
 
-        if (!owner || !repo) {
-            const error = new error({
-                message: "either owner or repo name is empty. exiting..."
-            });
-            errorHandler(error, core)
-        }
+        // if (!owner || !repo) {
+        //     const error = new error({
+        //         message: "either owner or repo name is empty. exiting..."
+        //     });
+        //     errorHandler(error, core)
+        // }
 
         const commonOpts = {
             host: "api.github.com",
@@ -37,47 +43,51 @@ async function run() {
             },
         };
 
-        const release = await getReleases(commonOpts, owner, repo).filter(release => release.name === releaseName)
+        const release = (await getReleases(commonOpts, owner, repo))
+          .filter(release => release.name === releaseName)
         await deleteRelease(commonOpts, owner, repo, release.id);
 
 
     } catch (ex) {
-        errorHandler(ex, core);
+        errorHandler(ex, core, 'run()');
     }
 }
 
 async function deleteTag(commonOpts, owner, repo, tagName) {
     return await fetch({
         ...commonOpts,
-        path: `/repos/${owner}/${repo}/git/refs/tags/${tagName}`,
+        url: `/repos/${owner}/${repo}/git/refs/tags/${tagName}`,
         method: "DELETE",
     })
-        .catch(error => errorHandler(error.message, core));;
+        .catch(error => errorHandler(error.message, core, 'deleteTag(commonOpts, owner, repo, tagName)'));;
 }
 
 async function deleteRelease(commonOpts, owner, repo, releaseId) {
     return await fetch({
         ...commonOpts,
-        path: `/repos/${owner}/${repo}/releases/${releaseId}`,
+        url: `/repos/${owner}/${repo}/releases/${releaseId}`,
         method: "DELETE",
     })
-        .catch(error => errorHandler(error.message, core));
+        .catch(error => errorHandler(error.message, core, 'deleteRelease(commonOpts, owner, repo, releaseId)'));
 }
 
 async function getReleases(commonOpts, owner, repo) {
     return await fetch({
         ...commonOpts,
-        path: `/repos/${owner}/${repo}/releases`,
+        url: `/repos/${owner}/${repo}/releases`,
         method: "GET",
     })
-        .catch(error => errorHandler(error.message));
+        .catch(error => {
+            console.log(error);
+            errorHandler(error.message, 'getReleases(commonOpts, owner, repo)');
+        });
 }
 
-function errorHandler(error, core) {
-    console.error(error.message)
+function errorHandler(error, core, functionName) {
+    console.error(error.message, functionName)
     process.exitCode = 1;
-    core.setFailed(error.message);
-    return;
+    // core.setFailed(error.message);
+    return error;
 }
 
 run();
