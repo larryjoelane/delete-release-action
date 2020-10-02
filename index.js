c20106367045bf2d560cf96ce455987ae55a94dc
Lane <llane@lockton.com>
2020-10-02 01:56:32 -0500

    version 1.2

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ try {
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
 
     if (!process.env.GITHUB_TOKEN) {
         console.error("no GITHUB_TOKEN found. pass `GITHUB_TOKEN` as env");
